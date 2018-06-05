4face17112b1fa8b72b208348f3c0ed3d5f30e10
im <marina.todorishina@mail.com>
2018-06-05 20:35:15 +0300

    generate feed service

---
+++ src/app/services/feed-service/feed-service.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class FeedServiceService {
+  private rssToJsonServiceBaseUrl = 'https://rss2json.com/api.json?rss_url=';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getFeedData(url: string) {
+    return this.http.get(this.rssToJsonServiceBaseUrl + url).map(this.extractFeeds);
+  }
+
+  private extractFeeds(res: Response) {
+    console.log(res);
+    return res || {};
+  }
+
+}
