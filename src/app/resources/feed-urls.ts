42c09aebb677d388bed873e1d32683a74fdb37c8
im <marina.todorishina@mail.com>
2018-06-05 20:35:44 +0300

    export array with rss urls

---
+++ src/app/resources/feed-urls.ts
@@ -0,0 +1,6 @@
+export const feedUrls = [
+  'http://www.ksl.com/xml/148.rss',
+  'http://www.npr.org/rss/rss.php?id=1001',
+  'http://www.npr.org/rss/rss.php?id=1013',
+  'http://feeds.sciencedaily.com/sciencedaily'
+];
