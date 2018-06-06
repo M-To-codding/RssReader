b8f08bd91a42afc031a52efc1f0753042910fa8b
im <marina.todorishina@mail.com>
2018-06-06 17:23:25 +0300

    create feed-item component for displaying feed messages

---
+++ src/app/components/feed-item/feed-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-item',
+  templateUrl: './feed-item.component.html',
+  styleUrls: ['./feed-item.component.css']
+})
+export class FeedItemComponent implements OnInit {
+  @Input() item;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
