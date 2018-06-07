679a76168802b1d2f727328406da63718ac5cc5a
im <marina.todorishina@mail.com>
2018-06-08 02:30:29 +0300

    add feed urls in array

--- src/app/resources/feed-urls.ts
+++ src/app/resources/feed-urls.ts
@@ -1,4 +1,7 @@
 export const feedUrls = [
+  'https://zik.ua/rss/export.rss',
+  'https://vokrugsveta.ua/feed',
+  'https://arzamas.academy/feed_v1.rss',
   'http://www.ksl.com/xml/148.rss',
   'http://www.npr.org/rss/rss.php?id=1001',
   'http://www.npr.org/rss/rss.php?id=1013',
