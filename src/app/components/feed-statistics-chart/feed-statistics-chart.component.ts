45c8a208547dd796faa4954b9d53dcd0f0920cef
im <marina.todorishina@mail.com>
2018-06-11 11:34:12 +0300

    refactor statistics chart component

--- src/app/components/feed-statistics-chart/feed-statistics-chart.component.ts
+++ src/app/components/feed-statistics-chart/feed-statistics-chart.component.ts
@@ -10,21 +10,12 @@ export class FeedStatisticsChartComponent {
   @Input() feedData;
 
   public chartLabels: string[] = ['All chars', 'Lat chars', 'Other chars'];
-  public chartData: number[] = [100, 0, 0];
   public chartType = 'pie';
   public chartOptions = {
     responsive: true
   };
 
   constructor() {
-  }
-
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
-
-  public chartHovered(e: any): void {
-    console.log(e);
   }
 
 }
